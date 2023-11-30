de87f0e0498b4847712f33ff15248e4a309d3504
Martehn03 <encantr@seznam.cz>
2023-11-30 16:05:47 +0100

    release 1.0

--- main_web.py
+++ main_web.py
@@ -177,4 +177,5 @@ def update_cards(_):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    #app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', debug=False, port=8055) #pro sdileni stranky v siti (testovano na windows)
